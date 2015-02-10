b534effc2338e97b48e68563b97de7b573b3bfd2
Kishan Karunaratne <kishan@karu.io>
2015-02-09 19:20:57 -0800

    schema creation

---
+++ setup/create_schema.rb
@@ -0,0 +1,73 @@
+require 'cassandra'
+
+cluster = Cassandra.cluster
+session = cluster.connect
+
+result = session.execute("SELECT * FROM system.schema_keyspaces WHERE keyspace_name='twissandra'").first
+
+unless result == nil
+	print "It looks like you have an existing twissandra schema and data. 
+          Do you want to delete and recreate it? (y/n): "
+  choice = gets
+  if choice == nil || choice[0] != 'y'
+    puts "Okay, no changes have been made. Exiting."
+    return
+  end
+
+  session.execute("DROP KEYSPACE twissandra")
+end
+
+# Create the keyspace
+session.execute("CREATE KEYSPACE twissandra WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'} ")
+
+# Create the tables
+session.execute("USE twissandra")
+
+session.execute("
+    CREATE TABLE users (
+        username text PRIMARY KEY,
+        password text)
+    ")
+
+session.execute("
+    CREATE TABLE friends (
+        username text,
+        friend text,
+        since timestamp,
+        PRIMARY KEY (username, friend))
+    ")
+
+session.execute("
+    CREATE TABLE followers (
+        username text,
+        follower text,
+        since timestamp,
+        PRIMARY KEY (username, follower))
+    ")
+
+session.execute("
+    CREATE TABLE tweets (
+        tweet_id uuid PRIMARY KEY,
+        username text,
+        body text)
+    ")
+
+session.execute("
+    CREATE TABLE usertweets (
+        username text,
+        time timeuuid,
+        tweet_id uuid,
+        PRIMARY KEY (username, time)
+    ) WITH CLUSTERING ORDER BY (time DESC)
+    ")
+
+session.execute("
+    CREATE TABLE timeline (
+        username text,
+        time timeuuid,
+        tweet_id uuid,
+        PRIMARY KEY (username, time)
+    ) WITH CLUSTERING ORDER BY (time DESC)
+    ")
+
+puts "Schema creation complete."
